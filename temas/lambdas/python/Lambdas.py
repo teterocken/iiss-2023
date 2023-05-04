038f7fbe28c7ccdfa9f381ae81e44fd969a2b20b
teterocken <117466557+teterocken@users.noreply.github.com>
2023-05-04 12:11:06 +0200

    Update Lambdas.py

--- temas/lambdas/python/Lambdas.py
+++ temas/lambdas/python/Lambdas.py
@@ -2,39 +2,36 @@ from functools import reduce
 # Funciones lambda basicas
 
 
-def a(x):
+def cuadrado(x):
     return x * x
 
 
-b = lambda x: x * x
+cuadrado_lambda = lambda x: x * x
 
 
-def c(x):
+def es_par(x):
     if x % 2 == 0:
         return True
     else:
         return False
 
 
-d = lambda x: True if x % 2 == 0 else False
+es_par_lambda = lambda x: True if x % 2 == 0 else False
 
 
-def e(x):
-    if x % 2 == 0:
-        return False
-    else:
-        return True
+def es_impar(x):
+    return not es_par(x)
 
 
-f = lambda x: False if x % 2 == 0 else True
+es_impar_lambda = lambda x: not es_par_lambda(x)
 
-
-print(a(3))
-print(b(3))
-print(c(100))
-print(d(3))
-print(e(100))
-print(f(3))
+print("Funciones lambda basicas:")
+print(cuadrado(3))
+print(cuadrado_lambda(3))
+print(es_par(100))
+print(es_par_lambda(3))
+print(es_impar(100))
+print(es_impar_lambda(3))
 
 
 # List comprehensions
@@ -48,6 +45,7 @@ for n in v:
 
 pares_lc = [x for x in v if x % 2 == 0]
 
+print("\nListas de pares de list comprehensions:")
 print(pares)
 print(pares_lc)
 
@@ -60,6 +58,7 @@ cuadrados = list(map(lambda x: x ** 2, numeros))
 
 sumacuadrados = reduce(lambda x, y: x + y, cuadrados)
 
+print("\nSuma de los cuadrados (map-reduce:")
 print(sumacuadrados)
 
 
@@ -69,4 +68,28 @@ vec = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 impares = list(filter(lambda x: x % 2 == 1, vec))
 
+print("\nLista de impares de los filtros")
 print(impares)
+
+
+# Tuple generators
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+suma_objetivo = 23
+
+generador_duplas = ((x, y) for i, x in enumerate(numeros) for y in numeros[i+1:] if x + y == suma_objetivo)
+
+print("\nDuplas de numeros que suman 23 con tuple generators")
+for dupla in generador_duplas:
+    print(dupla)
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+suma_objetivo = 23
+
+generador_tuplas = ((x, y, z) for i, x in enumerate(numeros) for j, y in enumerate(numeros[i+1:]) for z in
+                    numeros[i+j+2:] if x + y + z == suma_objetivo)
+
+print("\n3-tuplas de numeros que suman 23 con tuple generators")
+for tupla in generador_tuplas:
+    print(tupla)
